e7a593ee1448903100a543047191a91d23bf9fa7
Kristijan Maoduš <kmaodus@foi.hr>
2021-01-02 17:19:37 +0100

    First commit

---
+++ RentACarAsistent/RentACarAsistent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RentACarAsistent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
